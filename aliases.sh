14472c2c9e87855a4286a4a778fca91d284b671b
Dishank Bansal <dishankbansal09@gmail.com>
2025-03-05 17:28:23 +0000

    added bash custom prompt

--- aliases.sh
+++ aliases.sh
@@ -6,33 +6,20 @@ alias c='clear'
 # github aliases 
 alias gc='git commit'
 alias glog='git log'
-alias ga='git add'
+alias ga='git add -u'
 alias gs='git status'
 
 
 #venv aliases
 alias activate='source $(pwd)/.venv/bin/activate'
 
-# setting up the shell prompt
-parse_git_branch() {
-  git_branch=$(git symbolic-ref --short -q HEAD 2>/dev/null)
-  if [ -n "$git_branch" ]; then
-    git_status=$(git status --porcelain 2>/dev/null)
-    if [ -n "$git_status" ]; then
-      if echo "$git_status" | grep -q "^??"; then
-        git_color="\[\033[0;31m\]" # red for untracked files
-      else
-        git_color="\[\033[0;32m\]" # green for changes
-      fi
-    else
-      git_color="\[\033[0;36m\]" # cyan for clean
-    fi
-    echo " $git_color($git_branch)\[\033[0m\]"
-  fi
+# Custom bash Prompt
+# Function to get git branch info without extra spaces
+function parse_git_branch () {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-# Set the prompt
-export PS1='\[\033[0;33m\]\w\[\033[0m\]$(parse_git_branch)\[\033[0;32m\] \$\[\033[0m\] '
-
+# Set the prompt to include git branch information in red without leading or trailing space
+export PS1='\[\e[33m\]\w\[\e[m\]\[\e[31m\]$(parse_git_branch)\[\e[m\]\[\e[32m\]\$ \[\e[m\]'
 
 EOF
