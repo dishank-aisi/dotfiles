b48f5f12456f34c654b707f7fc5edd5fb5c66da5
Dishank Bansal <dishank.bansal@dsit.gov.uk>
2025-03-11 15:43:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,8 @@
 
 # clone dotfiles repo
 git clone https://github.com/dishank-aisi/dotfiles.git
-cd dotfiles
 
-cat aliases.sh >> $HOME/.bashrc
+cat dotfiles/aliases.sh >> $HOME/.bashrc
+
 source $HOME/.bashrc
-
-cd ..
-
 source dotfiles/control.sh
