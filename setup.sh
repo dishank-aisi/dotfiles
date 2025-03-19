e268651f093680c71a7efdb05e14dd4690d246a9
Dishank Bansal <dishank.bansal@dsit.gov.uk>
2025-03-19 14:50:43 +0000

    use ssh for cloning instead of https

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # This file to be used for AISI VM setting to be passed at setup script
 
 # cloning the dotfiles repo
-git clone https://github.com/dishank-aisi/dotfiles.git
+git clone git@github.com:dishank-aisi/dotfiles.git
 
 # setting up the bash
 cat dotfiles/aliases.sh >> $HOME/.bashrc
