0e5f809c2dfe3810da8b6a7ef190a2edd560dad3
Dishank Bansal <dishank.bansal@dsit.gov.uk>
2025-07-14 13:52:33 +0100

    changed sandbag to do uv sync

--- sandbag.sh
+++ sandbag.sh
@@ -5,6 +5,12 @@ echo "---Cloning the repo---"
 git clone git@github.com:dishank-aisi/sandbag-propensity.git --recurse-submodules
 cd sandbag-propensity
 
+# Installing everything
+# uv install everything
+echo "---running uv sync...----"
+uv sync --dev --all-extras
+uv pip install "git+ssh://git@github.com/AI-Safety-Institute/aisi-inspect-tools"
+
 # mount the s3 bucket
 sudo apt install -y s3fs
 mkdir logs
