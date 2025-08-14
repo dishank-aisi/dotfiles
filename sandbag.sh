5e9eb9a5c556de6bab7409b3997dd3765c681ecc
Dishank Bansal <dishank.bansal@dsit.gov.uk>
2025-08-14 17:56:31 +0100

    updated sandbag

--- sandbag.sh
+++ sandbag.sh
@@ -14,4 +14,6 @@ uv pip install "git+ssh://git@github.com/AI-Safety-Institute/aisi-inspect-tools"
 # mount the s3 bucket
 sudo apt install -y s3fs
 mkdir logs
-s3fs aisi-data-eu-west-2-prod:/teams/ru/agents/dishank/ ~/sandbag-propensity/logs -o iam_role=auto -o url=https://s3.eu-west-2.amazonaws.com -o endpoint=eu-west-2 -o use_path_request_style -o compat_dir
+s3fs aisi-data-eu-west-2-prod:/teams/cast/sandbagging/propensity ~/sandbag-propensity/logs -o iam_role=auto -o url=https://s3.eu-west-2.amazonaws.com -o endpoint=eu-west-2 -o use_path_request_style -o compat_dir
+
+touch ~/sangbagging_setup.txt
